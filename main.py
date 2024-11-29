75b1e6319424f3c30d4f9788f25ee6be428347d4
dlashock <dlashock@gmail.com>
2024-11-29 13:46:13 -0500

    fin

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ def main():
     book_path = "Books/frankenstein.txt"
     file_contents = extract_text(book_path)
     book_length = word_count(file_contents)
-    print(file_contents)
-    print(f"Wow! Frankenstein is {book_length} words long!")
+    letter_count = char_count(file_contents)
+    sorted_count = sorted_chars(letter_count)
+    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{book_length} words found in the document\n")
+    for count in sorted_count:
+        print(f"The '{count[0]}' character was found {count[1]} times")
+    print("--- End report ---")
 
 def extract_text(book):
     with open(book) as f:
@@ -11,5 +17,28 @@ def extract_text(book):
 
 def word_count(text):
     return len(text.split())
+
+def char_count(chars):
+    letters = {}
+    for char in chars:
+        char = char.lower()
+        if char.isalpha():
+            if char in letters:
+                letters[char] += 1
+            else:
+                letters[char] = 1
+    
+    return letters
+
+def sort_by(dict):
+    return dict[1]
+
+def sorted_chars(dict):
+    
+    list_of_dict = list(dict.items())
+    list_of_dict.sort(reverse=True, key=sort_by)
+    return list_of_dict
+    
+    #return list(sorted(dict.items(), key=lambda item: item[1], reverse=True))
 
 main()
